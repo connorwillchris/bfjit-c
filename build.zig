9921964ebc4873499e29c0371d3447201c832c99
JH 3 <jh3@dbhutah.org>
2025-06-05 11:39:58 -0600

    second commit

--- build.zig
+++ build.zig
@@ -3,8 +3,11 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const e = b.addExecutable(.{
         .name = "bfjit",
+        .target = b.graph.host,
     });
 
+    e.linkLibC();
+    b.installArtifact(e);
     e.addCSourceFiles(.{ .files = &.{
         "src/main.c",
     } });
