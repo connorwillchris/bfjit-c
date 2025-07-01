83ae7e3e5b94dcb39f5d51cbab88fa387f5f6781
Connor Christenson <connorwillchris@gmail.com>
2025-07-01 09:30:25 -0600

    made ptrs more consistent

--- src/bf.c
+++ src/bf.c
@@ -1,11 +1,11 @@
 #include "bf.h"
 
 struct Brainfuck {
-    char *tape;
+    char* tape;
     size_t len;
 };
 
-Bf * bf_new(size_t len) {
+Bf* bf_new(size_t len) {
     Bf* b;
     char* tape = (char*)malloc(len * sizeof(char));
     b = (Bf*)malloc(1 * sizeof(Bf));
